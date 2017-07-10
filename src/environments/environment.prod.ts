c881a5e80f31fd67d22b41dfa250da0b17f3c19e
Eswar <href2eswar@gmail.com>
2017-07-11 00:48:34 +0530

    openweatherapi url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
+  /**
+   * couldn't access http://api.openweathermap.org/data/2.5/forecast from github pages,
+   * throwing Mixed content error, So using nodejs in middle to bypass
+   */
   openWeatherAPI: 'https://immense-cliffs-51527.herokuapp.com/'
 };
